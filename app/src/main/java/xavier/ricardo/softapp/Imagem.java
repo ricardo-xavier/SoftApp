788d15c8ce323514895726773a71dcabd0c0fc0a
ricardo-xavier <ricardo.costa.xavier@gmail.com>
2020-01-18 20:01:46 -0300

    android studio

---
+++ app/src/main/java/xavier/ricardo/softapp/Imagem.java
@@ -0,0 +1,18 @@
+package xavier.ricardo.softapp;
+
+public class Imagem {
+    private String image64;
+    private String id;
+    public String getImage64() {
+        return image64;
+    }
+    public void setImage64(String image64) {
+        this.image64 = image64;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+}
